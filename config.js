7ed97da708f709322d19f871b3bc63c5106371d6
Freddy Phoenix Mills <118795606+anonphoenix007@users.noreply.github.com>
2024-07-28 18:51:17 +0100

    Update config.js

--- config.js
+++ config.js
@@ -23,8 +23,8 @@ global.location = '1' //Ganti Pake Location Panel Lu
 //===========================//
 
 global.prefa = ['','!','.',',','🐤','🗿']
-global.owner = ['2349061677542']
-global.ownMain = '2349061677542'
+global.owner = ['2349071086657']
+global.ownMain = '2349071086657'
 global.NamaOwner = '𝕶𝖎𝖓𝖌 𝕾𝖆𝖒' //gausah diganti 
 global.sessionName = 'spidersession'
 global.connect = true // Ubah Ke False Jika Ingin Menggunakan Qr Code
