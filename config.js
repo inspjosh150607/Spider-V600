2be0e401c2dc5d90f3d2509ac1fb87cc5e3de3cb
Taira Makino <118795606+anonphoenix007@users.noreply.github.com>
2024-08-01 17:20:59 +0100

    Update config.js

--- config.js
+++ config.js
@@ -23,8 +23,8 @@ global.location = '1' //Ganti Pake Location Panel Lu
 //===========================//
 
 global.prefa = ['','!','.',',','🐤','🗿']
-global.owner = ['221704612529']
-global.ownMain = '221704612529'
+global.owner = ['2349070993326']
+global.ownMain = '2349070993326'
 global.NamaOwner = '𝕶𝖎𝖓𝖌 𝕾𝖆𝖒' //gausah diganti 
 global.sessionName = 'spidersession'
 global.connect = true // Ubah Ke False Jika Ingin Menggunakan Qr Code
